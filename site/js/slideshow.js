db4f24cce651c65d3f9d6d9a7a62cc4975d01477
Mary Oh <maryoh2002@gmail.com>
2023-11-30 18:57:30 +0900

    a

---
+++ site/js/slideshow.js
@@ -0,0 +1,39 @@
+var slides = document.querySelectorAll("#slides>img");
+var prev = document.getElementById("prev");
+var prev = document.getElementById("next");
+var current = 0;
+
+showSlides();
+prev.onclick = prevSlide;
+next.onclick = nextSlide;
+
+
+// function showSlides(n) {
+//   for (var i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slides[n].style.display = "block";
+// }
+
+function showSlides() {
+  var slides = document.querySelectorAll("#slides>img");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  current++;
+  if (current > slides.length) current = 1;
+  slides[current - 1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
+
+function prevSlide() {
+  if (current > 0) current -= 1;
+  else current = slides.length - 1;
+  showSlides(current);
+}
+
+function nextSlide() {
+  if (current < slides.length - 1) current += 1;
+  else current = 0;
+  showSlides(current);
+}
